6c12dad365c477bb6a7548628ba19515491fa25f
Joseph Giunta <jgnohack@gmail.com>
2020-12-18 15:18:11 -0800

    add 2nd and 3rd tests to vendor tests

--- VendorAndOrderTracker.Tests/Models.Tests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/Models.Tests/VendorTests.cs
@@ -8,12 +8,29 @@ namespace VendorAndOrderTracker.Tests
   [TestClass]
   public class VendorTests
   {
-   [TestMethod]
+    [TestMethod]
     public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_VendorName()
     {
       Order newOrder = new Order("title", "Description", 19, 3, 5, "2020-12-18"); // Title, description, price, bread amount, pastry amount, date
       Vendor newVendor = new Vendor("Suzie's Cafe", "Description", newOrder); // VendorName, description, Order list
       Assert.AreEqual("Suzie's Cafe", newVendor.VendorName);
+    }
+
+    [TestMethod]
+    public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_Description()
+    {
+      Order newOrder = new Order("title", "Description", 19, 3, 5, "2020-12-18"); // Title, description, price, bread amount, pastry amount, date
+      Vendor newVendor = new Vendor("Suzie's Cafe", "Description", newOrder); // VendorName, description, Order list
+      Assert.AreEqual("Description", newVendor.Description);
+    }
+
+    [TestMethod]
+    public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_OrderList()
+    {
+      Order newOrder = new Order("title", "Description", 19, 3, 5, "2020-12-18"); // Title, description, price, bread amount, pastry amount, date
+      Vendor newVendor = new Vendor("Suzie's Cafe", "Description"); // VendorName, description, Order list
+      newVendor.Add(newOrder);
+      Assert.AreEqual(newOrder, newVendor.OrderList[0]);
     }
 
   }
