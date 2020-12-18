69f544c768758d5eafe8d34cfced646c4870356d
Joseph Giunta <jgnohack@gmail.com>
2020-12-18 14:42:34 -0800

    add order constructor, lots of reading

--- VendorAndOrderTracker/Models/Order.cs
+++ VendorAndOrderTracker/Models/Order.cs
@@ -4,6 +4,21 @@ namespace VendorAndOrderTracker.Models
 {
   public class Order
   {
-    
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int BreadAmount { get; set; }
+    public int PastryAmount { get; set; }
+    public string Date { get; set; }
+
+    public Order(string title, string description, int price, int breadAmount, int pastryAmount, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      BreadAmount = breadAmount;
+      PastryAmount = pastryAmount;
+      Date = date;
+    }
   }
 }
