19a464d1ea5e545ac85e3bf84d75fa5c9e2816ef
Joseph Giunta <jgnohack@gmail.com>
2020-12-18 15:40:30 -0800

    update vendor tests

--- VendorAndOrderTracker.Tests/Models.Tests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/Models.Tests/VendorTests.cs
@@ -12,23 +12,33 @@ namespace VendorAndOrderTracker.Tests
     public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_VendorName()
     {
       Vendor newVendor = new Vendor("Suzie's Cafe", "Description"); // VendorName, description, Order list
-      Assert.AreEqual("Suzie's Cafe", newVendor.VendorName);
+
+      string result = newVendor.VendorName;
+
+      Assert.AreEqual("Suzie's Cafe", result);
     }
 
     [TestMethod]
     public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_Description()
     {
       Vendor newVendor = new Vendor("Suzie's Cafe", "Description"); // VendorName, description, Order list
-      Assert.AreEqual("Description", newVendor.Description);
+
+      string result = newVendor.Description;
+
+      Assert.AreEqual("Description", result);
     }
 
     [TestMethod]
-    public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_OrderList()
+    public void AddOrder_AssociatesOrderWithVendor_OrderList()
     {
       Order newOrder = new Order("title", "Description", 19, 3, 5, "2020-12-18"); // Title, description, price, bread amount, pastry amount, date
       Vendor newVendor = new Vendor("Suzie's Cafe", "Description"); // VendorName, description, Order list
-      newVendor.Add(newOrder);
-      Assert.AreEqual(newOrder, newVendor.OrderList[0]);
+      newVendor.AddOrder(newOrder);
+
+      List<Order> result = newVendor.Orders;
+
+
+      Assert.AreEqual(newOrder, result);
     }
 
   }
--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -4,6 +4,16 @@ namespace VendorAndOrderTracker.Models
 {
   public class Vendor
   {
-    
+    public List<Order> Orders { get; set; }
+    public string VendorName { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+  
+  public Vendor(string vendorName, string description)
+  {
+    VendorName = vendorName;
+    Description = description;
+    Orders = new List<Order>{};
+  }
   }
 }
