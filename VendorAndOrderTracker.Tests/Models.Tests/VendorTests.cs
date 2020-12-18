3cd7a98cf70f1684b970248f8c1820de9218c33d
Joseph Giunta <jgnohack@gmail.com>
2020-12-18 14:49:40 -0800

    add first vendor constructor test

--- VendorAndOrderTracker.Tests/Models.Tests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/Models.Tests/VendorTests.cs
@@ -8,7 +8,13 @@ namespace VendorAndOrderTracker.Tests
   [TestClass]
   public class VendorTests
   {
-   
+   [TestMethod]
+    public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_VendorName()
+    {
+      Order newOrder = new Order("title", "Description", 19, 3, 5, "2020-12-18"); // Title, description, price, bread amount, pastry amount, date
+      Vendor newVendor = new Vendor("Suzie's Cafe", "Description", newOrder); // VendorName, description, Order list
+      Assert.AreEqual("Suzie's Cafe", newVendor.VendorName);
+    }
 
   }
 }
