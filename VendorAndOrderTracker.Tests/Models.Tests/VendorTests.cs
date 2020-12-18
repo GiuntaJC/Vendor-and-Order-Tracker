b820ccb00c736cbb6d03619570d201b09dcccedb
Joseph Giunta <jgnohack@gmail.com>
2020-12-18 15:18:59 -0800

    update vendor tests

--- VendorAndOrderTracker.Tests/Models.Tests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/Models.Tests/VendorTests.cs
@@ -11,16 +11,14 @@ namespace VendorAndOrderTracker.Tests
     [TestMethod]
     public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_VendorName()
     {
-      Order newOrder = new Order("title", "Description", 19, 3, 5, "2020-12-18"); // Title, description, price, bread amount, pastry amount, date
-      Vendor newVendor = new Vendor("Suzie's Cafe", "Description", newOrder); // VendorName, description, Order list
+      Vendor newVendor = new Vendor("Suzie's Cafe", "Description"); // VendorName, description, Order list
       Assert.AreEqual("Suzie's Cafe", newVendor.VendorName);
     }
 
     [TestMethod]
     public void VendorConstructor_ConfirmPropertyMatchesIntendedValue_Description()
     {
-      Order newOrder = new Order("title", "Description", 19, 3, 5, "2020-12-18"); // Title, description, price, bread amount, pastry amount, date
-      Vendor newVendor = new Vendor("Suzie's Cafe", "Description", newOrder); // VendorName, description, Order list
+      Vendor newVendor = new Vendor("Suzie's Cafe", "Description"); // VendorName, description, Order list
       Assert.AreEqual("Description", newVendor.Description);
     }
 
